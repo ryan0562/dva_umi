1fa4e273080e5d0bb24dff58622549ff983e76e3
wangwei <wang-wei@hzzh.com>
2021-03-01 17:42:52 +0800

    初始化

--- .umirc.ts
+++ .umirc.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'umi';
+import  routes  from './config/routes.js';
 
 export default defineConfig({
   nodeModulesTransform: {
     type: 'none',
   },
-  routes: [
-    { path: '/', component: '@/pages/index' },
-  ],
+  routes,
   fastRefresh: {},
 });
